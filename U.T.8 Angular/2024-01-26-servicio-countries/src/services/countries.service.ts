b2b72b0119af91389c20139ba553797538f374aa
alemilla04 <ale.vmv2004@gmail.com>
2024-01-25 12:19:10 +0100

    countries

--- U.T.8 Angular/2024-01-26-servicio-countries/src/services/countries.service.ts
+++ U.T.8 Angular/2024-01-26-servicio-countries/src/services/countries.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class CountriesService {
 
   constructor(private _http: HttpClient) { }
 
-  public retrieveCountries(): any{
+  public retrieveCountries(): Observable<Object>{
     const url = 'https://restcountries.com/v3.1/all';
     const countries = this._http.get(url);
     return countries;
